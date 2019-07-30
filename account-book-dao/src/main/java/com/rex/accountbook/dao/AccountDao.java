faba9b267414993651091d6e76b9c0be2bf47daf
Rex Yu <yu.chenhang@gmail.com>
2019-07-31 00:31:22 +0800

    edit AccountDao

--- account-book-dao/src/main/java/com/rex/accountbook/dao/AccountDao.java
+++ account-book-dao/src/main/java/com/rex/accountbook/dao/AccountDao.java
@@ -6,17 +6,21 @@ import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.Table;
+import java.math.BigDecimal;
 
-// TODO 未完
 @Getter
 @Setter
 @Entity
-@Table(name = "account")
+@Table(name = "account", indexes = {@Index(name = "account_type_index", columnList = "name,type", unique = true)})
 public class AccountDao extends BaseDao {
 
-    // 名稱
     @Column(length = 10, nullable = false)
     private String name;
+    @Column(length = 1, nullable = false)
+    private String type;
+    @Column(nullable = false, scale = 2)
+    private BigDecimal initAmount = BigDecimal.ZERO;
 
 }
