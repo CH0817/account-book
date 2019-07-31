a55c4775a6c22519b2a2c2dc434928a44392b47a
Rex Yu <yu.chenhang@gmail.com>
2019-07-31 09:47:36 +0800

    remove @ToString

--- account-book-dao/src/main/java/com/rex/accountbook/dao/TradeDao.java
+++ account-book-dao/src/main/java/com/rex/accountbook/dao/TradeDao.java
@@ -3,13 +3,11 @@ package com.rex.accountbook.dao;
 import com.rex.accountbook.dao.base.BaseDao;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@ToString
 @Getter
 @Setter
 @Entity
