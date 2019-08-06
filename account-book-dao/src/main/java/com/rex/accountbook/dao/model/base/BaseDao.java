1d6778f6d567bdf66c6924480086e9fa98d102b5
Rex Yu <yu.chenhang@gmail.com>
2019-08-06 11:11:03 +0800

    @ToString instead of apache commons ToStringBuilder

--- account-book-dao/src/main/java/com/rex/accountbook/dao/model/base/BaseDao.java
+++ account-book-dao/src/main/java/com/rex/accountbook/dao/model/base/BaseDao.java
@@ -2,8 +2,7 @@ package com.rex.accountbook.dao.model.base;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
+import lombok.ToString;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,6 +12,7 @@ import java.io.Serializable;
 
 @Getter
 @Setter
+@ToString
 @MappedSuperclass
 public class BaseDao implements Serializable {
 
@@ -20,8 +20,4 @@ public class BaseDao implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Override
-    public String toString() {
-        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
-    }
 }
