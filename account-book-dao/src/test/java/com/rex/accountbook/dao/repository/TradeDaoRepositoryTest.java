3af19157785b54223474fa694542523e957801c8
Rex Yu <yu.chenhang@gmail.com>
2019-08-06 16:55:11 +0800

    create BaseRepositoryTest

--- account-book-dao/src/test/java/com/rex/accountbook/dao/repository/TradeDaoRepositoryTest.java
+++ account-book-dao/src/test/java/com/rex/accountbook/dao/repository/TradeDaoRepositoryTest.java
@@ -1,30 +1,29 @@
 package com.rex.accountbook.dao.repository;
 
-import com.rex.accountbook.dao.model.TradeDao;
+import com.rex.accountbook.dao.repository.base.BaseRepositoryTest;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.jdbc.Sql;
 
-@RunWith(SpringRunner.class)
-// 此註解只會掃描 @Entity 的 Bean 和 裝配 Spring Data JPA 存儲庫，
-// 其他常規 @Service @Component @Repository 註解的 Bean 不會加載到 Spring context
-@DataJpaTest
-// 載入測試 properties
-// @TestPropertySource("classpath:db-info.properties")
-// 不使用內嵌資料庫
-// @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class TradeDaoRepositoryTest {
+// 測試資料
+@Sql({"classpath:data/account_type.sql", "classpath:data/account.sql", "classpath:data/trade_type.sql", "classpath:data/item.sql"})
+public class TradeDaoRepositoryTest extends BaseRepositoryTest {
 
     @Autowired
-    private TradeDaoRepository repository;
+    private AccountDaoRepository accountDaoRepository;
+    @Autowired
+    private AccountTypeDaoRepository accountTypeDaoRepository;
+    @Autowired
+    private ItemDaoRepository itemDaoRepository;
+    @Autowired
+    private TradeDaoRepository tradeDaoRepository;
+    @Autowired
+    private TradeTypeDaoRepository tradeTypeDaoRepository;
 
-    @Test(expected = DataIntegrityViolationException.class)
-    public void saveWithNotFullData() {
+    @Test
+    public void save() {
         // do test
-        repository.save(new TradeDao());
+        // repository.save(new TradeDao());
     }
 
 }
