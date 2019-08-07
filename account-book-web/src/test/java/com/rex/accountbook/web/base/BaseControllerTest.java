ea49fe6451141a1c4445195c16f175592eb90afb
Rex Yu <yu.chenhang@gmail.com>
2019-08-07 16:54:44 +0800

    add @Transactional and @Ignore

--- account-book-web/src/test/java/com/rex/accountbook/web/base/BaseControllerTest.java
+++ account-book-web/src/test/java/com/rex/accountbook/web/base/BaseControllerTest.java
@@ -1,6 +1,7 @@
 package com.rex.accountbook.web.base;
 
 import com.rex.accountbook.AccountBookApplication;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,10 +10,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {AccountBookApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@Transactional
+@Ignore
 public class BaseControllerTest {
 
     protected Logger logger = LoggerFactory.getLogger(getClass());
